c9cd7d7b909c608248f5794b21395b3985a38d44
HansAdam2077 <s245623@dtu.dk>
2026-03-20 16:50:37 +0100

    Update loop range and log output in test.py

--- test/test.py
+++ test/test.py
@@ -43,8 +43,8 @@ async def test_project(dut):
         await ClockCycles(dut.clk, 4)
     
 
-    for i in range(23):
-        dut._log.info(f"uo_out = {dut.uio_out.value} ({int(dut.uio_out.value)})")
+    for i in range(41):
+        dut._log.info(f"uo_out = {dut.uo_out.value} ({int(dut.uo_out.value)})")
         await ClockCycles(dut.clk, 4)
         
     
